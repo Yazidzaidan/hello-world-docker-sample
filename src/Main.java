384da6347f9be2c3633205c64748986ed43f9fef
Yazidzaidan <yazidzaidanz@gmail.com>
2022-04-10 01:23:23 +0800

    Fiveth Commit

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("Thank you");
         System.out.println("Thank you sir");
+        System.out.println("you welcome!!!");
 
         /*
         int count = 0;
