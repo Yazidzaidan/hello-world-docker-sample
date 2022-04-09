b36e1bafe9e803832815e27538567437e913837d
Yazidzaidan <yazidzaidanz@gmail.com>
2022-04-10 00:18:21 +0800

    First commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World!");
+        System.out.println("Thank you");
+
+        /*
+        int count = 0;
+        try{
+            while (true){
+                Thread.sleep(2+1000);
+                System.out.println("I'm still here! Iteration" + count++);
+            }
+
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
+         */
+    }
+}
